330d794f3686b0310204e6069acb92d30d045266
Stefan Pavlovic <stefan.pavlovic@ingsoftware.com>
2019-11-27 17:06:41 +0100

    Added configuration for entity models

---
+++ Infrastructure/Configuration/CustomerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(c => c.FirstName)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(c => c.LastName)
+              .IsRequired()
+              .HasMaxLength(30);
+        }
+    }
+}
---
+++ Infrastructure/Configuration/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
